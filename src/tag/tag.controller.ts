739d5fb048c6d0c0815732da86c897cb6f9d9db4
Ashot Israelyan <ashot@picsart.com>
2023-07-12 17:21:06 +0400

    Controller modified to send tag information from database

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
+import { TagEntity } from '@app/tag/tag.entity';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll() {
+  findAll(): Promise<TagEntity[]> {
     return this.tagService.findAll();
   }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['dragons', 'coffee'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  findAll(): Promise<TagEntity[]> {
+    return this.tagRepository.find();
   }
 }
