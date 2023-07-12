0bd78544f33a1f940446da1f07eeb948bb3fca88
Ashot Israelyan <ashot@picsart.com>
2023-07-12 15:32:10 +0400

    Postgress integrated with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
