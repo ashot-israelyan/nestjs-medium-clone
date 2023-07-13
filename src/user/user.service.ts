2a1729d5571640e9caffd723c7da7351dc3c201d
Ashot Israelyan <ashot@picsart.com>
2023-07-13 12:11:04 +0400

    Update user functionality added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,6 +8,7 @@ import { JWT_SECRET } from '@app/config';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 import { LoginUserDto } from '@app/user/dto/loginUser.dto';
 import { compare } from 'bcrypt';
+import { UpdateUserDto } from '@app/user/dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
@@ -67,6 +68,15 @@ export class UserService {
 
     delete user.password;
     return user;
+  }
+
+  async updateUser(
+    userId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserEntity> {
+    const user = await this.findById(userId);
+    Object.assign(user, updateUserDto);
+    return this.userRepository.save(user);
   }
 
   generateJwt(user: UserEntity): string {
