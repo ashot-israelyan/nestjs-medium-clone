c19221cb03abf7d5e2d968e1b4a8c467c7cfdda2
Ashot Israelyan <ashot@picsart.com>
2023-07-13 14:06:28 +0400

    Slug generation implemented

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -4,6 +4,8 @@ import { ArticleEntity } from '@app/article/article.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '@app/user/user.entity';
+import { ArticleResponseInterface } from '@app/article/types/articleResponse.interface';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticleService {
@@ -11,6 +13,7 @@ export class ArticleService {
     @InjectRepository(ArticleEntity)
     private readonly articleRepository: Repository<ArticleEntity>,
   ) {}
+
   async createArticle(
     currentUser: UserEntity,
     createArticleDto: CreateArticleDto,
@@ -21,10 +24,22 @@ export class ArticleService {
       article.tagList = [];
     }
 
-    article.slug = 'foo';
+    article.slug = this.getSlug(createArticleDto.title);
 
     article.author = currentUser;
 
     return this.articleRepository.save(article);
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
   }
 }
