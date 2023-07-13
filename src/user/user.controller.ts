d7bd2697d344a0f5b303c7591d164072c05e2748
Ashot Israelyan <ashot@picsart.com>
2023-07-13 10:59:52 +0400

    Login implemented

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,16 +8,26 @@ import {
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+import { LoginUserDto } from '@app/user/dto/loginUser.dto';
 
-@Controller()
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Post('users')
+  @Post()
   @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginDto);
     return this.userService.buildUserResponse(user);
   }
 }
