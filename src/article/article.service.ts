26b18a85f89573d9b33e7ada50ca9dc4f3908a40
Ashot Israelyan <ashot@picsart.com>
2023-07-14 10:02:56 +0400

    Article favorite functionality added

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -98,6 +98,31 @@ export class ArticleService {
     return this.articleRepository.findOne({ slug });
   }
 
+  async addArticleToFavorites(
+    slug: string,
+    userId: number,
+  ): Promise<ArticleEntity> {
+    const article = await this.findBySlug(slug);
+    const user = await this.userRepository.findOne(userId, {
+      relations: ['favorites'],
+    });
+
+    const isNotFavorited =
+      user.favorites.findIndex(
+        (articleInFavorites) => articleInFavorites.id === article.id,
+      ) === -1;
+
+    if (isNotFavorited) {
+      user.favorites.push(article);
+      article.favouritesCount++;
+
+      await this.userRepository.save(user);
+      await this.articleRepository.save(article);
+    }
+
+    return article;
+  }
+
   async deleteArticle(
     slug: string,
     currentUserId: number,
