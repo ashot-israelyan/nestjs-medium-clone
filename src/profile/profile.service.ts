935e9c62e590d0fe3674257018ecca4e7ddc855a
Ashot Israelyan <ashot@picsart.com>
2023-07-14 12:09:34 +0400

    Profile follow functionality implemented

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,13 +4,17 @@ import { ProfileType } from '@app/profile/types/profile.type';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@app/user/user.entity';
 import { Repository } from 'typeorm';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
+
   async getProfile(
     currentUserId: number,
     profileUsername: string,
@@ -26,7 +30,42 @@ export class ProfileService {
     return { ...user, following: false };
   }
 
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and Following cant be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
+  }
+
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    delete profile.email;
     return {
       profile,
     };
